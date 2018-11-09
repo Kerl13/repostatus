5a4bb59cb6908cc3328964423b5b69499f2aab5c
Martin Pépin <kerl@wkerl.me>
2018-11-09 20:50:24 +0100

    explore folders recursively

--- repostatus.sh
+++ repostatus.sh
@@ -9,17 +9,23 @@ set -euC
 
 HERE="$HOME/git"
 
-SUCCESS_COLOR="\033[32m"
-FAIL_COLOR="\033[31m"
-REPO_COLOR="\033[34;1m"  # bold blue
-WHITE="\033[0m"
+SUCCESS_COLOR="\\033[32m"
+FAIL_COLOR="\\033[31m"
+REPO_COLOR="\\033[34;1m"  # bold blue
+WHITE="\\033[0m"
+
+
+trim_prefix () {
+  echo "${1#$HERE/}"
+}
 
 
 print_repo () {
   pad="==============================================================="
-  printf '==( %b%s%b )' "$REPO_COLOR" "$repo" "$WHITE"
-  printf '%*.*s' 0 $((60 - ${#1} - 6 - ${#3} - 6)) "$pad"
-  printf '( %b%s%b )==' "$2" "$3" "$WHITE"
+  reponame="$(trim_prefix "$(pwd)")"
+  printf '==( %b%s%b )' "$REPO_COLOR" "$reponame" "$WHITE"
+  printf '%*.*s' 0 $((60 - ${#reponame} - 6 - ${#2} - 6)) "$pad"
+  printf '( %b%s%b )==' "$1" "$2" "$WHITE"
   printf '\n'
 }
 
@@ -33,25 +39,41 @@ finish () {
 
 
 check () {
-  cd "$repo"
   if ! remote=$(git config --get branch.master.remote); then
       remote="origin"
   fi
   status="$(git status --porcelain)"
   diff="$(git log "$remote"/master..master)"
   if [ "_$status" = "_" ] && [ "_$diff" = "_" ]; then
-    print_repo "$repo" "$SUCCESS_COLOR" Ok
+    print_repo "$SUCCESS_COLOR" Ok
   else
-    print_repo "$repo" "$FAIL_COLOR" Failed
+    print_repo "$FAIL_COLOR" Failed
     git status
   fi
-  cd "$HERE"
 }
 
-cd "$HERE"
-find . -path '*/*' -prune -type d | while read -r repo
-do
-  check "$repo"
-done
+plouf () {
+  before_plouf="$(pwd)"
+  cd "$1"
+  if [ -d .git ]; then
+    check
+  else
+    if [ "$(pwd)" != "$HERE" ]; then
+      print_repo "$SUCCESS_COLOR" "↺"
+    fi
+    find . -path '*/*' -prune -type d | while read -r repo
+    do
+      plouf "$repo"
+    done
+  fi
+  cd "$before_plouf"
+}
+
+plouf "$HERE"
+
+#find . -path '*/*' -prune -type d | while read -r repo
+#do
+#  check "$repo"
+#done
 
 finish
